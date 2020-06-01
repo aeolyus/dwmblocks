7e99218151068ea65bb53c3380100ad1bac3b965
Richard Huang <richardhuang.huang@gmail.com>
2020-05-31 22:13:23 -0700

    make delimeters smaller

--- blocks.h
+++ blocks.h
@@ -3,15 +3,15 @@ static const Block blocks[] = {
   /* icon */ /* command */ /* update interval */ /* update signal */
   {"net: ", "nmcli | (rg 'connected to' || echo disconnected) | sed 's/.*connected to //g'", 5, 0},
 
-  {"",      "echo ' | '", 0, 0},
+  {"",      "echo '|'", 0, 0},
 
   {"vol: ", "amixer get Master | tail -n1 | grep -o \"\\[.*\\]\"", 0, 1},
 
-  {"",      "echo ' | '", 0, 0},
+  {"",      "echo '|'", 0, 0},
 
   {"bat: ", "upower -i $(upower -e | rg BAT0) | rg -om1 '.*%' | awk '{print $2}'", 60, 0},
 
-  {"",      "echo ' | '", 0, 0},
+  {"",      "echo '|'", 0, 0},
 
   {"",      "date +\'%a %F %H:%M\'", 60, 0},
 };
